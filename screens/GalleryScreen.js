57dda590c4e6ae6389b894a3f08eb94a57e5141a
asiphile <asiphilemthethethwa@gmail.com>
2024-12-03 09:04:38 +0200

    adding images functionality

---
+++ screens/GalleryScreen.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import { View, FlatList, Image, TouchableOpacity, StyleSheet } from 'react-native';
+import { fetchImages } from '../database/SQLiteDatabase';
+
+const GalleryScreen = ({ navigation }) => {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    fetchImages(setImages);
+  }, []);
+
+  const renderItem = ({ item }) => (
+    <TouchableOpacity onPress={() => navigation.navigate('ImageDetails', { image: item })}>
+      <Image source={{ uri: item.uri }} style={styles.image} />
+    </TouchableOpacity>
+  );
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={images}
+        keyExtractor={(item) => item.id.toString()}
+        numColumns={3}
+        renderItem={renderItem}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: { flex: 1, padding: 5 },
+  image: { width: 100, height: 100, margin: 5 },
+});
+
+export default GalleryScreen;
